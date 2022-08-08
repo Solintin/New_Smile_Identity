534a2fbd859a3a201166d8be78e97ecb15b368bc
DevSaalih <soliualley@gmail.com>
2022-08-08 16:09:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const getNav = document.querySelector(".header_content-mobile");
+const navButton = document.querySelector("#toggleButton");
+
+navButton.addEventListener("click", () => {
+  getNav.classList.toggle("open");
+});
+
+AOS.init();
